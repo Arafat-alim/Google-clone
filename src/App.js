564f6b83ae9e414b12234f504add685f8a688f47
arafat-alim <arafat97alim@gmail.com>
2022-07-19 23:46:06 +0530

    funny quotes added.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1>Google Me swagat hain</h1>
+      <h1>Google Me Apka swagat Hain</h1>
     </div>
   );
 }
